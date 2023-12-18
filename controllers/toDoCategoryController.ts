9180c90c3644ed2e45a0fecf9691c8689ac76a75
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-12-18 18:51:33 -0500

    UPDATE - create category: create only if there is no duplicate name

--- controllers/toDoCategoryController.ts
+++ controllers/toDoCategoryController.ts
@@ -40,6 +40,16 @@ const createCategory = async (req: AuthRequest, res: Response) => {
     const { color, icon, isEditable, name }: IToDoCategory = req.body;
     const { user } = req;
 
+    // Check if a category with the same name already exists
+    const existingCategory = await ToDoCategory.findOne({ name, user });
+
+    if (existingCategory) {
+      // If the category exists, send an appropriate response
+      return res
+        .status(400)
+        .send({ error: "Category with this name already exists" });
+    }
+
     const category = await ToDoCategory.create({
       color,
       icon,
