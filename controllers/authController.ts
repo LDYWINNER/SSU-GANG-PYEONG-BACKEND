9cd3c12f0c470a8576b39768b373cb471f57c382
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-03-02 03:48:05 -0500

    SETUP - email

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -3,12 +3,83 @@ import { AuthRequest } from "../middleware/authenticateUser";
 import User from "../models/User";
 import { StatusCodes } from "http-status-codes";
 import { createJWT } from "../utils/tokenUtils";
-import { BadRequestError } from "../errors";
+import { BadRequestError, UnAuthenticatedError } from "../errors";
+import ejs from "ejs";
+import nodemailer from "nodemailer";
+import path from "path";
+
+const appDir = path.dirname(require.main?.filename as string);
+
+let registerEmailConfirmationNum = 0;
+let loginEmailConfirmationNum = 0;
+let loginUserEmail = "";
+
+// for email
+const generateRandom = (min: number, max: number) => {
+  const ranNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  return ranNum;
+};
+
+const registerEmail = async (req: Request, res: Response) => {
+  //   const { username, email, school, major } = req.body;
+  //   if (!username || !email || school === "-1" || major === "-2") {
+  //     throw new BadRequestError("Please check if you provided all values");
+  //   }
+  //   //duplicate email checking
+  //   const userAlreadyExists = await User.findOne({ email });
+  //   if (userAlreadyExists) {
+  //     throw new BadRequestError("Email already in use");
+  //   }
+  //   userData = {
+  //     username,
+  //     email,
+  //     school,
+  //     major,
+  //   };
+  //   let authNum = generateRandom(111111, 999999);
+  //   let emailTemplete;
+  //   ejs.renderFile(
+  //     appDir + "/template/authMail.ejs",
+  //     { authCode: authNum },
+  //     function (err, data) {
+  //       if (err) {
+  //         console.log(err);
+  //       }
+  //       emailTemplete = data;
+  //     }
+  //   );
+  //   let transporter = nodemailer.createTransport({
+  //     service: "naver",
+  //     host: "smtp.naver.com",
+  //     port: 465,
+  //     auth: {
+  //       user: process.env.NODEMAILER_USER,
+  //       pass: process.env.NODEMAILER_PASS,
+  //     },
+  //     tls: {
+  //       rejectUnauthorized: false,
+  //     },
+  //   });
+  //   let mailOptions = await transporter.sendMail({
+  //     from: process.env.NODEMAILER_USER,
+  //     to: req.body.email,
+  //     subject: "SUNYTIME Register Email Verfication",
+  //     html: emailTemplete,
+  //   });
+  //   registerEmailConfirmationNum = authNum;
+  //   transporter.sendMail(mailOptions, function (error, info) {
+  //     if (error) {
+  //       console.log(error);
+  //     }
+  //     // console.log("Finish sending email : " + info.response);
+  //     res.send({ registerEmailConfirmationNum });
+  //     transporter.close();
+  //   });
+};
 
 const register = async (req: Request, res: Response) => {
   try {
     const { username, email, school, major } = req.body;
-
     //check if already exist user
     const existingUser = await User.findOne({ email });
     if (existingUser) {
@@ -16,7 +87,6 @@ const register = async (req: Request, res: Response) => {
         .status(StatusCodes.CONFLICT)
         .send({ message: "User already exist with this email" });
     }
-
     const user = await User.create({
       username,
       email,
@@ -28,7 +98,6 @@ const register = async (req: Request, res: Response) => {
       },
       personalSchedule: [],
     });
-
     return res.status(StatusCodes.CREATED).json({
       user: {
         _id: user._id,
@@ -45,6 +114,73 @@ const register = async (req: Request, res: Response) => {
     console.log("error in register", error);
     throw error;
   }
+};
+
+const loginEmail = async (req: Request, res: Response) => {
+  const { email }: { email: string } = req.body;
+  const lowerCaseEmail = email.toLowerCase();
+
+  if (!email) {
+    throw new BadRequestError("Please provide valid email");
+  }
+  const user = await User.findOne({ email: lowerCaseEmail });
+  if (!user) {
+    throw new UnAuthenticatedError("Login failed");
+  }
+  // console.log(user);
+  loginUserEmail = lowerCaseEmail;
+
+  if (user.adminAccount) {
+    return res.send({ authNum: -1, loginSkip: true });
+  }
+
+  //send email
+  let authNum = generateRandom(1, 99);
+  let emailTemplete;
+  ejs.renderFile(
+    appDir + "/template/loginMail.ejs",
+    { authCode: authNum },
+    function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      emailTemplete = data;
+    }
+  );
+
+  let transporter = nodemailer.createTransport({
+    service: "naver",
+    host: "smtp.naver.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  let mailOptions = await transporter.sendMail({
+    from: `"SSUGANGPYEONG" <${process.env.NODEMAILER_USER}>`,
+    to: req.body.email,
+    subject: "SSUGANGPYEONG Login Email Verfication",
+    html: emailTemplete,
+  });
+
+  loginEmailConfirmationNum = authNum;
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    }
+    console.log("Finish sending email : " + info.response);
+    console.log("loginEmailConfirmationNum : ", loginEmailConfirmationNum);
+
+    res.send({ authNum: loginEmailConfirmationNum });
+    transporter.close();
+  });
 };
 
 const login = async (req: Request, res: Response) => {
@@ -96,4 +232,4 @@ const updateUser = async (req: AuthRequest, res: Response) => {
   res.status(StatusCodes.OK).json({ db_user, token });
 };
 
-export { register, login, updateUser };
+export { registerEmail, register, loginEmail, login, updateUser };
--- routers/authRouter.ts
+++ routers/authRouter.ts
@@ -2,10 +2,25 @@ import express from "express";
 
 const authRouter = express.Router();
 
-import { register, login, updateUser } from "../controllers/authController";
+import rateLimiter from "express-rate-limit";
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
 
-authRouter.route("/register").post(register);
-authRouter.route("/login").post(login);
-authRouter.route("/updateUser").patch(updateUser);
+import {
+  register,
+  login,
+  updateUser,
+  registerEmail,
+  loginEmail,
+} from "../controllers/authController";
+
+authRouter.route("/sendEmail").post(apiLimiter, registerEmail);
+authRouter.route("/loginEmail").post(apiLimiter, loginEmail);
+authRouter.route("/register").post(apiLimiter, register);
+authRouter.route("/login").post(apiLimiter, login);
+authRouter.route("/updateUser").patch(apiLimiter, updateUser);
 
 export default authRouter;
